50b99e7a72360a89ce35ebaf48dd035512edda5b
mstuper <mstuper@uncc.edu>
2023-01-19 15:04:07 -0500

    getting docker to work

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:17
+FROM node:12
 
 WORKDIR /app
 
@@ -8,8 +8,8 @@ RUN npm install
 
 COPY . .
 
-ENV PORT=3000
+ENV PORT=8080
 
-EXPOSE 3000
+EXPOSE 8080
 
 CMD [ "npm", "start" ]
--- node.js
+++ node.js
@@ -3,10 +3,10 @@ var app = express();
 
 
 app.get('/', (req,res) => {
-    res.send('Hello World')
+    res.json('Hello World')
     
 });
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 
-app.listen(port, () => console.log('Listening on http://localhost:${3000}'));
+app.listen(port, () => console.log('Listening on http://localhost:${port}'));
