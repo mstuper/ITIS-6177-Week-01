07c135abba6da0807d84ccad8d7824e678860773
mstuper <mstuper@uncc.edu>
2023-01-19 13:54:53 -0500

    updating the env

--- node.js
+++ node.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
-var port = 3000
+
 
 app.get('/', (req,res) => {
     res.send('Hello World')
     
 });
 
+var port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log('Listening on http://localhost:${3000}'));
